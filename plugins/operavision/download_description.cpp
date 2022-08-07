7742a9c4ab4fef5738551fa80925100db9bfd942
The Artful Bodger <TheArtfulBodger@users.noreply.github.com>
2022-08-07 19:37:00 +0100

    Added code for Operavision Downloader

---
+++ plugins/operavision/download_description.cpp
@@ -0,0 +1,122 @@
+#include <cpr/cpr.h>
+
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+
+#include <td/spec/v1.hpp>
+
+#include <Document.h>
+#include <Node.h>
+#include <Selection.h>
+
+#include <tinyxml2.h>
+
+#include "opera.hpp"
+
+void download_description(const td::dl& base, const td::job& job)
+{
+    auto o = msgpack::unpack<opera>(job->get_job_data());
+
+    // Get page for performace on
+    auto uri = "https://operavision.eu/performance/" + o.slug;
+    auto rsp = cpr::Get(cpr::Url { uri });
+    CDocument doc;
+    doc.parse(rsp.text);
+
+    tinyxml2::XMLDocument xdoc = tinyxml2::XMLDocument();
+    auto decl = xdoc.NewDeclaration(nullptr);
+    xdoc.LinkEndChild(decl);
+
+    auto video = xdoc.NewElement("video");
+    xdoc.LinkEndChild(video);
+
+    // UniqueID
+    {
+        auto elem = xdoc.NewElement("uniqueid");
+        video->LinkEndChild(elem);
+
+        elem->SetAttribute("type", "ovdl");
+        elem->SetText(uri.c_str());
+    }
+
+    // Title
+    {
+        auto elem = xdoc.NewElement("title");
+        video->LinkEndChild(elem);
+        elem->SetText(o.name.c_str());
+    }
+
+    // Outline
+    {
+        auto elem = xdoc.NewElement("outline");
+        video->LinkEndChild(elem);
+
+        auto outline = doc.find("p.intro")
+                           .nodeAt(0)
+                           .text();
+
+        elem->SetText(outline.c_str());
+    }
+
+    // Plot
+    {
+        auto elem = xdoc.NewElement("plot");
+        video->LinkEndChild(elem);
+
+        auto nodes = doc.find("p.intro")
+                         .nodeAt(0)
+                         .parent()
+                         .find("p:not(.intro)");
+
+        std::string plot;
+        for (int i = 0; i < nodes.nodeNum(); i++) {
+            plot += nodes.nodeAt(i).text() + "\n\n";
+        }
+
+        elem->SetText(plot.c_str());
+    }
+
+    // Cast
+    {
+        auto nodes = doc.find(".castTable .castRow");
+        for (int i = 0; i < nodes.nodeNum(); i++) {
+            auto elem = xdoc.NewElement("actor");
+            video->LinkEndChild(elem);
+
+            auto elem2 = xdoc.NewElement("name");
+            elem->LinkEndChild(elem2);
+
+            auto elem3 = xdoc.NewElement("role");
+            elem->LinkEndChild(elem3);
+
+            auto th = nodes.nodeAt(i).childAt(0).text();
+            elem3->SetText(th.c_str());
+
+            auto td = nodes.nodeAt(i).childAt(1).text();
+            elem2->SetText(td.c_str());
+        }
+    }
+
+    // Time
+    {
+        auto elem = xdoc.NewElement("date");
+        video->LinkEndChild(elem);
+
+        auto date = doc.find(".performancedate time")
+                        .nodeAt(0)
+                        .attribute("datetime");
+
+        elem->SetText(date.c_str());
+    }
+
+    // Write To Disk
+    auto folder = std::filesystem::path(base->get_outpath_folder()) / o.company / o.name;
+    auto outfile = folder / (o.slug + ".nfo");
+    std::filesystem::create_directories(folder);
+
+    xdoc.SaveFile(outfile.c_str());
+
+    std::string data = "\x80";
+    job->set_complete(td::buffer(data.begin(), data.end()), false);
+}
---
+++ plugins/operavision/should_skip.cpp
@@ -0,0 +1,52 @@
+#include <filesystem>
+#include <iostream>
+
+#include <td/spec/v1.hpp>
+
+#include "opera.hpp"
+
+bool should_skip(const td::dl& base, const td::job& job)
+{
+    // decode job
+    auto o = msgpack::unpack<opera>(job->get_job_data());
+
+    switch (o.operation) {
+    case download_mode_video: {
+        auto folder = std::filesystem::path(base->get_outpath_folder()) / o.company / o.name;
+        auto mp4 = folder / (o.slug + ".mp4");
+        auto mkv = folder / (o.slug + ".mkv");
+        auto webm = folder / (o.slug + ".webm");
+        return std::filesystem::exists(mp4)
+            or std::filesystem::exists(mkv)
+            or std::filesystem::exists(webm);
+    }
+    case download_mode_desc: {
+        auto path = std::filesystem::path(base->get_outpath_folder()) / o.company / o.name / (o.slug + ".nfo");
+        return std::filesystem::exists(path);
+    }
+    default:
+        return true;
+    }
+}
+
+void download_description(const td::dl& base, const td::job& job);
+void download_video(const td::dl& base, const td::job& job);
+
+void process_job(const td::dl& base, const td::job& job)
+{
+    auto o = msgpack::unpack<opera>(job->get_job_data());
+
+    switch (o.operation) {
+    case download_mode_video:
+        download_video(base, job);
+        break;
+    case download_mode_desc:
+        download_description(base, job);
+        break;
+
+    default: {
+        std::string data = "\xbfUnknown Value for download mode";
+        job->set_complete(td::buffer(data.begin(), data.end()), true);
+    }
+    }
+}
