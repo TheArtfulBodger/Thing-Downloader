a8c5ec24a20f76d4ca18ddd191a95e8171bcd926
The Artful Bodger <TheArtfulBodger@users.noreply.github.com>
2022-10-02 12:06:04 +0100

    Give Windows ZLIB as external_project

---
+++ ext/zlib.cmake
@@ -0,0 +1,17 @@
+if(MSVC)
+    ExternalProject_Add(zlib
+    GIT_REPOSITORY "${git_protocol}://github.com/madler/zlib.git"
+    GIT_TAG "v1.2.12"
+    SOURCE_DIR zlib
+    BINARY_DIR zlib-build
+    INSTALL_DIR zlib-install
+    CMAKE_CACHE_ARGS
+        ${ep_common_args}
+        -DZLIB_MANGLE_PREFIX:STRING=itkexamples_zlib_
+        -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
+    )
+    set(zlib_DIR "${CMAKE_BINARY_DIR}/zlib-install" CACHE PATH "zlib dir" FORCE)
+    set(ZLIB_ROOT ${zlib_DIR} CACHE PATH "zlib root" FORCE)
+    set(ZLIB_INCLUDE_DIR ${zlib_DIR}/include CACHE PATH "zlib include dir" FORCE)
+    set(ZLIB_LIBRARY ${zlib_DIR}/lib/zlib.lib CACHE FILEPATH "zlib library" FORCE)
+endif()
