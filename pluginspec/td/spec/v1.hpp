f69e0d694b8c283e5591d7e779d549f65f1a62b1
The Artful Bodger <TheArtfulBodger@users.noreply.github.com>
2022-10-02 14:08:24 +0100

    Add DLLEXPORT

--- pluginspec/td/spec/v1.hpp
+++ pluginspec/td/spec/v1.hpp
@@ -2,6 +2,12 @@
 #include <memory>
 #include <string>
 #include <vector>
+ 
+#if defined(_MSC_VER) && !defined(_DLL) 
+    /*Enabled as "export" while compiling the dll project*/
+    #define DLLEXPORT __declspec(dllexport)  
+#endif
+
 
 namespace td {
 
@@ -83,22 +89,22 @@ extern "C" {
 // List of Symbols required for a plugin
 
 /*! @brief Version of the specification implemented.*/
-extern const int version;
+DLLEXPORT extern const int version;
 
 /*! @brief *Unique* identifier of the plugin. Should only contain lowercase letters, numbers and hyphens.*/
-extern std::string key;
+DLLEXPORT extern std::string key;
 
 /*! @brief Name of the plugin in the UI, can contain any characters. */
-extern std::string name;
+DLLEXPORT extern std::string name;
 
 /*! @brief Description of the plugin for the UI, can contain any characters. */
-extern std::string description;
+DLLEXPORT extern std::string description;
 
 /*! @brief List of required configuration variables for the plugin to function. */
-extern std::vector<std::string> required_confs;
+DLLEXPORT extern std::vector<std::string> required_confs;
 
 /*! @brief List of required secrets for the plugin to function. */
-extern std::vector<std::string> required_secrets;
+DLLEXPORT extern std::vector<std::string> required_secrets;
 
 // WARNING: These Functions should operate as fully encapsulated functions, using no global state, and all job properties should be stored in the buffer, and secrets and configuration in the downloader object.
 
@@ -108,14 +114,14 @@ extern std::vector<std::string> required_secrets;
  * Jobs should be added via the td::downloader_base::add_job function
  * @param base Smart pointer to the base downloader object (td::downloader_base)
  */
-void get_jobs(const td::dl& base);
+DLLEXPORT void get_jobs(const td::dl& base);
 
 /*!
  * @brief Executes the download job.
  * @param base Smart pointer to the base downloader object (td::downloader_base)
  * @param job Smart pointer to the job object (td::job_base)
  */
-void process_job(const td::dl& base, const td::job& job);
+DLLEXPORT void process_job(const td::dl& base, const td::job& job);
 
 /*!
  * @brief Checks if the job has been previously executed to avoid redownloading files that already exists.
@@ -124,7 +130,7 @@ void process_job(const td::dl& base, const td::job& job);
  * @return `false` If the job should continue
  * @return `true` If the job has been executed successfully previously
  */
-bool should_skip(const td::dl& base, const td::job& job);
+DLLEXPORT bool should_skip(const td::dl& base, const td::job& job);
 }
 
 // NoDocsPage
