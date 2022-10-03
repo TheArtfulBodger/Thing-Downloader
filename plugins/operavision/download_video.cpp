27e5039f9dbd63f78bd069d5f062fae1fb48d9ac
The Artful Bodger <TheArtfulBodger@users.noreply.github.com>
2022-10-03 13:59:54 +0100

    replaced subprocess library

--- plugins/operavision/download_video.cpp
+++ plugins/operavision/download_video.cpp
@@ -1,66 +1,58 @@
 #include <cpr/cpr.h>
 
 #include <filesystem>
+#include <iostream>
 #include <regex>
+#include <sstream>
 #include <td/spec/v1.hpp>
 
-#include <subprocess.hpp>
+#include <process.hpp>
 
 #include "opera.hpp"
 
-// yt-dl uses frequent carrige returns to reset the line, this is an alternative implementation to std::getline which accounts for both
-bool get_line(std::istream& stream, std::string& delims, std::string& line)
-{
-    line.erase();
-    char c;
-    while (stream.get(c)) {
-        if (delims.find(c) != std::string::npos) {
-            return true;
-        }
-        line += c;
-    }
-    return false;
-}
-
 void download_video(const td::dl& base, const td::job& job)
 {
+
+    auto r1 = std::regex("\\[download\\][ ]+([0-9.]+)% of ([0-9.]+)(B|KiB|MiB|GiB) at[ ]+([0-9.]+)(B|KiB|MiB|GiB)/s ETA ([0-9:]+)");
+    // Add More Regex for more output lines
+
     auto o = nlohmann::json::parse(job->get_job_data()).get<opera>();
 
     auto folder = std::filesystem::path(base->get_outpath_folder()) / o.company / o.name;
     std::filesystem::create_directories(folder);
 
-    subprocess::popen cmd(base->get_conf("YOUTUBE_DL_PATH"),
-        { "--write-sub",
+    std::stringstream stdout;
+
+    TinyProcessLib::Process cmd(
+        std::vector<std::string> { base->get_conf("YOUTUBE_DL_PATH"),
+            "--write-sub",
             "--all-subs",
             "--embed-subs",
             "https://youtu.be/" + o.video_id, "-o", folder.string() + "/" + o.slug + ".%(ext)s",
-            "--restrict-filenames" });
+            "--restrict-filenames" },
+        "",
+        [&](const char* bytes, size_t n) {
+            std::string line(bytes, n);
+            std::smatch m;
 
-    auto r1 = std::regex("\\[download\\][ ]+([0-9.]+)% of ([0-9.]+)(B|KiB|MiB|GiB) at[ ]+([0-9.]+)(B|KiB|MiB|GiB)/s ETA ([0-9:]+)");
-    // Add More Regex for more output lines
+            std::regex_search(line, m, r1);
+            if (!m.empty()) {
 
-    std::string delims = "\n\r";
-    for (std::string line; get_line(cmd.stdout(), delims, line);) {
-        std::smatch m;
+                job->set_progress(std::stof(m[1]));
+                std::string status(m[1]);
+                status += "% of ";
+                status += m[2];
+                status += m[3];
+                status += " (";
+                status += m[6];
+                status += ")";
+                job->set_status(status);
+            } else {
+                // Check more regex Lines
+            }
+        });
 
-        std::regex_search(line, m, r1);
-        if (!m.empty()) {
+    int i = cmd.get_exit_status();
 
-            job->set_progress(std::stof(m[1]));
-            std::string status(m[1]);
-            status += "% of ";
-            status += m[2];
-            status += m[3];
-            status += " (";
-            status += m[6];
-            status += ")";
-            job->set_status(status);
-        } else {
-            // Check more regex Lines
-        }
-    }
-
-    cmd.wait();
-
-    job->set_complete(job->get_job_data(), false);
+    job->set_complete(job->get_job_data(), i != 0);
 }
